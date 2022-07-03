dc1bef1868a6ccfb5d300d2cd538753d3a0ea804
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-07-03 09:22:08 +0100

    Landlord registration

---
+++ web/Controllers/LandlordRegistrationController.cs
@@ -0,0 +1,47 @@
+﻿using BricksAndHearts.Database;
+using BricksAndHearts.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BricksAndHearts.Controllers;
+
+[Authorize]
+[Route("/landlord/register")]
+public class LandlordRegistrationController: AbstractController
+{
+    private readonly BricksAndHeartsDbContext _dbContext;
+    public LandlordRegistrationController(BricksAndHeartsDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public ActionResult Index()
+    {
+        return View("Create", new LandlordCreateModel
+        {
+            Email = GetCurrentUser().GoogleEmail
+        });
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult> Create([FromForm] LandlordCreateModel createModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("Create");
+        }
+
+        var dbModel = new LandlordDbModel
+        {
+            FirstName = createModel.FirstName,
+            LastName = createModel.LastName,
+        };
+        _dbContext.Landlords.Add(dbModel);
+        await _dbContext.SaveChangesAsync();
+
+        return Redirect("/landlord/me/profile"); // qq
+    }
+}
---
+++ web/ViewModels/LandlordCreateModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BricksAndHearts.ViewModels;
+
+public class LandlordCreateModel
+{
+    [Required]
+    [StringLength(60)]
+    [DisplayName("Title")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(255)]
+    [DisplayName("First Name")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(255)]
+    [DisplayName("Last Name")]
+    public string LastName { get; set; } = string.Empty;
+
+    [StringLength(1000)]
+    [DisplayName("Company")]
+    public string? CompanyName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Phone]
+    public string Phone { get; set; } = string.Empty;
+}
