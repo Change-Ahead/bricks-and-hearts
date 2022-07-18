7b3023bcc83bfd7305dd557a3504d850905fef9f
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-18 14:43:15 +0100

    BNH-22 feat: Created PropertyDbModel.

---
+++ web/Database/PropertyDbModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BricksAndHearts.Database;
+
+public class PropertyDbModel
+{
+    public int Id { get; set; }
+    public virtual LandlordDbModel Landlord { get; set; }
+    public string Address { get; set; }
+}
