1dfafbabeeb30ec3408944a65b7c9fef4302d6d4
Max Johnson <max.johnson@softwire.com>
2022-07-19 12:19:32 +0100

    BNH-23 feat: Expand a property's address into separate fields

--- web/ViewModels/PropertyViewModel.cs
+++ web/ViewModels/PropertyViewModel.cs
@@ -5,13 +5,31 @@ namespace BricksAndHearts.ViewModels;
 
 public class PropertyViewModel
 {
-    [Required] public string Address { get; set; } = string.Empty;
+    [Required] public PropertyAddress Address { get; set; }
 
     public static PropertyViewModel FromDbModel(PropertyDbModel property)
     {
         return new PropertyViewModel
         {
-            Address = property.Address,
+            Address = new PropertyAddress
+            {
+                AddressLine1 = property.AddressLine1,
+                AddressLine2 = property.AddressLine2,
+                AddressLine3 = property.AddressLine3,
+                TownOrCity = property.TownOrCity,
+                County = property.County,
+                Postcode = property.Postcode
+            }
         };
     }
+}
+
+public class PropertyAddress
+{
+    public string AddressLine1 { get; set; }
+    public string AddressLine2 { get; set; }
+    public string AddressLine3 { get; set; }
+    public string TownOrCity { get; set; }
+    public string County { get; set; }
+    public string Postcode { get; set; }
 }
