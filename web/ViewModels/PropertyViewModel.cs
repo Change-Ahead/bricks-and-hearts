a537a6035d4c1914735fb9393c8f42c6db425082
Max Johnson <max.johnson@softwire.com>
2022-07-21 09:51:37 +0100

    BNH-23 style: Add string length validation to property fields

--- web/ViewModels/PropertyViewModel.cs
+++ web/ViewModels/PropertyViewModel.cs
@@ -8,20 +8,20 @@ public class PropertyViewModel
 {
     [Required] public PropertyAddress Address { get; set; }
 
-    [Required] public string PropertyType { get; set; } = string.Empty;
+    [Required] [StringLength(10000)] public string PropertyType { get; set; } = string.Empty;
 
     [Required]
-    [Range(0, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+    [Range(0, 1000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
     [DisplayName("Number of Bedrooms")]
     public int NumOfBedrooms { get; set; }
 
     public DateTime CreationTime { get; set; } = DateTime.Now;
 
     [Required]
-    [Range(0, 10000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+    [Range(0, 100000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
     public int Rent { get; set; }
 
-    [Required] public string Description { get; set; } = string.Empty;
+    [Required] [StringLength(20000)] public string Description { get; set; } = string.Empty;
 
     public static PropertyViewModel FromDbModel(PropertyDbModel property)
     {
@@ -47,10 +47,15 @@ public class PropertyViewModel
 
 public class PropertyAddress
 {
-    [Required] public string AddressLine1 { get; set; } = string.Empty;
-    public string? AddressLine2 { get; set; }
-    public string? AddressLine3 { get; set; }
-    [Required] public string TownOrCity { get; set; } = string.Empty;
-    [Required] public string County { get; set; } = string.Empty;
-    [Required] public string Postcode { get; set; } = string.Empty;
+    [Required] [StringLength(10000)] public string AddressLine1 { get; set; } = string.Empty;
+
+    [StringLength(10000)] public string? AddressLine2 { get; set; }
+
+    [StringLength(10000)] public string? AddressLine3 { get; set; }
+
+    [Required] [StringLength(10000)] public string TownOrCity { get; set; } = string.Empty;
+
+    [Required] [StringLength(10000)] public string County { get; set; } = string.Empty;
+
+    [Required] [StringLength(100)] public string Postcode { get; set; } = string.Empty;
 }
