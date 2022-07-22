292d1907118789ce499d6ddec69adb58f51fc3de
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-22 16:03:34 +0100

    BNH-33 feat: Create PostcodeApiResponseViewModel.

---
+++ web/ViewModels/PostcodeApiResponseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace BricksAndHearts.ViewModels;
+
+public class PostcodeApiResponseViewModel
+{
+    [JsonProperty("results")]
+    public List<Results>? ListOfResults { get; set; }
+}
+
+public class Results
+{
+    [JsonProperty("address")] 
+    public Dictionary<string, string>? Address { get; set; }
+    [JsonProperty("position")] 
+    public Dictionary<string,string>? LatLon { get; set; }
+}
