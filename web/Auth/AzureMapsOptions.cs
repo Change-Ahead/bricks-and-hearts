8624f208cfa2f477550ce2b79903a3bf04d926ce
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-22 16:03:13 +0100

    BNH-33 feat: Create AzureMapsOptions for storing subscription key for Azure Maps.

---
+++ web/Auth/AzureMapsOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace BricksAndHearts.Auth;
+
+public class AzureMapsOptions
+{
+    public const string AzureMaps = "AzureMaps";
+    public string SubscriptionKey { get; set; } = String.Empty;
+}
