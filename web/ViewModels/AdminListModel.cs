96a107d8d882a5b23ba53473d3718e7454d812c9
Glenn Clarke <Glenn.Clarke@softwire.com>
2022-07-22 10:50:29 +0100

    BNH-49 refactor: Fixed a compiler warning surrounding AdminListModel.cs

--- web/ViewModels/AdminListModel.cs
+++ web/ViewModels/AdminListModel.cs
@@ -4,6 +4,12 @@ namespace BricksAndHearts.ViewModels;
 
 public class AdminListModel
 {
+    public AdminListModel(List<UserDbModel> currentAdmins, List<UserDbModel> pendingAdmins)
+    {
+        CurrentAdmins = currentAdmins;
+        PendingAdmins = pendingAdmins;
+    }
+
     public List<UserDbModel> CurrentAdmins { get; set; }
     public List<UserDbModel> PendingAdmins { get; set; }
 }
