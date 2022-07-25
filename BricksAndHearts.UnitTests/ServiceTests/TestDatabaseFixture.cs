7ad0872d3839b7bf366f98413f9557cb9ba33938
Max Johnson <max.johnson@softwire.com>
2022-07-25 17:41:35 +0100

    BNH-61 test: Set up context and fixture framework for database tests

---
+++ BricksAndHearts.UnitTests/ServiceTests/TestDatabaseFixture.cs
@@ -0,0 +1,83 @@
+﻿using BricksAndHearts.Database;
+
+namespace BricksAndHearts.UnitTests.ServiceTests;
+
+public class TestDatabaseFixture
+{
+    private static readonly object Lock = new();
+    private static bool _databaseInitialised;
+
+    public TestDatabaseFixture()
+    {
+        lock (Lock)
+        {
+            if (_databaseInitialised) return;
+            using (var context = new TestDbContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Users.AddRange(
+                    CreateAdminUser(),
+                    CreateNonAdminUser(),
+                    CreateRequestedAdminUser());
+                context.SaveChanges();
+            }
+
+            _databaseInitialised = true;
+        }
+    }
+
+    public BricksAndHeartsDbContext CreateReadContext()
+    {
+        return new TestDbContext();
+    }
+
+    public BricksAndHeartsDbContext CreateWriteContext()
+    {
+        var context = new TestDbContext();
+        // Begin a transaction so the test's writes don't interfere with other tests running in parallel (provides test isolation)
+        // Transaction is never committed so is automatically rolled back at the end of the test
+        context.Database.BeginTransaction();
+        return context;
+    }
+
+    public UserDbModel CreateAdminUser()
+    {
+        return new UserDbModel
+        {
+            GoogleUserName = "AdminUser",
+            GoogleEmail = "test.email@gmail.com",
+            GoogleAccountId = "1",
+            IsAdmin = true,
+            LandlordId = null,
+            HasRequestedAdmin = false
+        };
+    }
+
+    public UserDbModel CreateNonAdminUser()
+    {
+        return new UserDbModel
+        {
+            GoogleUserName = "NonAdminUser",
+            GoogleEmail = "test.email2@gmail.com",
+            GoogleAccountId = "2",
+            IsAdmin = false,
+            LandlordId = null,
+            HasRequestedAdmin = false
+        };
+    }
+
+    public UserDbModel CreateRequestedAdminUser()
+    {
+        return new UserDbModel
+        {
+            GoogleUserName = "HasRequestedAdminUser",
+            GoogleEmail = "test.email3@gmail.com",
+            GoogleAccountId = "3",
+            IsAdmin = false,
+            LandlordId = null,
+            HasRequestedAdmin = true
+        };
+    }
+}
---
+++ BricksAndHearts.UnitTests/TestDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BricksAndHearts.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace BricksAndHearts.UnitTests;
+
+public class TestDbContext : BricksAndHeartsDbContext
+{
+    private const string ConnectionString =
+        "Server=(local);Database=BricksAndHeartsTest;Trusted_Connection=True;Integrated Security=True;";
+
+    public TestDbContext() : base(null!)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionString);
+    }
+}
