91ec6a4f5ed5b74d28cf3f9aeddc3f6377718e56
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-08-01 15:18:07 +0100

    BNH-65 feat: Change ToAddress from a single string to a list of strings.

--- web/Auth/EmailConfigOptions.cs
+++ web/Auth/EmailConfigOptions.cs
@@ -4,10 +4,10 @@ public class EmailConfigOptions
 {
     public const string Email = "Email";
 
-    public string Host { get; set; } = String.Empty;
+    public string Host { get; set; } = string.Empty;
     public int Port { get; set; }
-    public string UserName { get; set; } = String.Empty;
-    public string Password { get; set; } = String.Empty;
-    public string FromAddress { get; set; } = String.Empty;
-    public string ToAddress { get; set; } = String.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string FromAddress { get; set; } = string.Empty;
+    public List<string> ToAddress { get; set; } = new ();
 }
