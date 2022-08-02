b733ebccfd801aa96f70c8e1e75f6106fe48624e
Max Johnson <max.johnson@softwire.com>
2022-08-02 16:43:49 +0100

    BNH-82 feat: Add database migration

---
+++ web/Migrations/20220802154106_AddTenantProfileToProperty.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BricksAndHearts.Migrations
+{
+    public partial class AddTenantProfileToProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsBenefits",
+                table: "Property",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsCouple",
+                table: "Property",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsFamily",
+                table: "Property",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsNotEET",
+                table: "Property",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsPets",
+                table: "Property",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsSingleTenant",
+                table: "Property",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsWithoutGuarantor",
+                table: "Property",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptsBenefits",
+                table: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "AcceptsCouple",
+                table: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "AcceptsFamily",
+                table: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "AcceptsNotEET",
+                table: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "AcceptsPets",
+                table: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "AcceptsSingleTenant",
+                table: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "AcceptsWithoutGuarantor",
+                table: "Property");
+        }
+    }
+}
