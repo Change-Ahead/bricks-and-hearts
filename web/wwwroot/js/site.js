6b2f5a33621ab5b95d6b60729318506991bc47d8
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-08-11 10:56:55 +0100

    BNH-123 refactor: Awaited an async javascript function, small amount of code restyling

--- web/wwwroot/js/site.js
+++ web/wwwroot/js/site.js
@@ -19,10 +19,12 @@ function checkIfSelectedValueIsTarget(target){
 
 function copyLinkToClipboard(linkToCopy){
     // Copy the text passed into the function plus the rest of the url
-    let fullLink = window.location.origin+"/invite/"+linkToCopy
-    navigator.clipboard.writeText(fullLink);
-    
-    // Change the button styling to show the link has been copied
-    document.getElementById("copyBtn").innerHTML = "<i class='bi bi-check'></i> invite link copied"
-    document.getElementById("copyBtn").className = "btn btn-success my-3"
+    const fullLink = window.location.origin+"/invite/"+linkToCopy
+    navigator.clipboard.writeText(fullLink)
+    .then(() => {
+        // Change the button styling to show the link has been copied
+        const copyBtn = document.getElementById("copyBtn");
+        copyBtn.innerHTML = "<i class='bi bi-check'></i> invite link copied"
+        copyBtn.className = "btn btn-success my-3"
+    });
 }
