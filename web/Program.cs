1e1f7a9560a990a627ee269541eca8a4a8896ace
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2022-08-12 12:48:46 +0100

    Fix exception handler

--- web/Program.cs
+++ web/Program.cs
@@ -63,7 +63,7 @@ if (app.Configuration.GetValue<bool>("MigrateOnStartup"))
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error/500");
+    app.UseExceptionHandler("/Error/500");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
