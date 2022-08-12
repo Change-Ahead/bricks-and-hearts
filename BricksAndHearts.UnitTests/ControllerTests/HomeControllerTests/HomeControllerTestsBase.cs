4586f9b65e314e65e6c4b2db3dd65deda60f0ccf
Claire Carlotti <Claire.Calotti@softwire.com>
2022-08-12 12:00:48 +0100

    BNH-109 test: add unit tests for Index redirection

---
+++ BricksAndHearts.UnitTests/ControllerTests/HomeControllerTests/HomeControllerTestsBase.cs
@@ -0,0 +1,17 @@
+﻿using BricksAndHearts.Controllers;
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+
+namespace BricksAndHearts.UnitTests.ControllerTests.HomeControllerTests;
+
+public class HomeControllerTestsBase : ControllerTestsBase
+{
+    protected readonly ILogger<HomeController> Logger;
+    protected readonly HomeController UnderTest;
+
+    protected HomeControllerTestsBase()
+    {
+        Logger = A.Fake<ILogger<HomeController>>();
+        UnderTest = new HomeController(Logger);
+    }
+}
