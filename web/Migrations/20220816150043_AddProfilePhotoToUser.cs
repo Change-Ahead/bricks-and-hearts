f00109c00088f194e22925849344689901704ea2
Max Johnson <max.johnson@softwire.com>
2022-08-16 16:51:18 +0100

    BNH-99 chore: Database migration

---
+++ web/Migrations/20220816150043_AddProfilePhotoToUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BricksAndHearts.Migrations
+{
+    public partial class AddProfilePhotoToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleProfileImageUrl",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GoogleProfileImageUrl",
+                table: "User");
+        }
+    }
+}
