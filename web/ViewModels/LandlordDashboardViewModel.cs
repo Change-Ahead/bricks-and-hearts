af56303df2cfecbb3e8ea78eceded88a662139c7
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-08-16 17:46:53 +0100

    BNH-139 feat: restyling main dashboard page to remove ugliness

--- web/ViewModels/LandlordDashboardViewModel.cs
+++ web/ViewModels/LandlordDashboardViewModel.cs
@@ -3,5 +3,6 @@
 public class LandlordDashboardViewModel
 {
     public List<PropertyDetailsViewModel>? Properties { get; set; }
+    public PropertyCountModel? PropertyTypeCount { get; set; }
     public LandlordProfileModel CurrentLandlord { get; set; } = new();
 }
