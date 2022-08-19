0ef1779f4c232be5cea8a6b8f7c8fc5ae33e9e85
Max Johnson <max.johnson@softwire.com>
2022-08-19 16:29:47 +0100

    BNH-162 feat: Implement copy shareable link

--- web/wwwroot/js/propertyDetails.js
+++ web/wwwroot/js/propertyDetails.js
@@ -14,3 +14,11 @@ deleteImageModal.addEventListener('hide.bs.modal', function () {
     const imageModal = document.getElementById('imageModal');
     imageModal.classList.toggle("modal-overlay");
 });
+
+function copyShareableLink(token) {
+    const fullLink = window.location.origin + "/property/public/" + token
+    navigator.clipboard.writeText(fullLink).then(() => {
+        const copyBtn = document.getElementById("copyBtn");
+        copyBtn.innerHTML = "<i class='bi bi-check-lg'></i> Link copied";
+    });
+}
